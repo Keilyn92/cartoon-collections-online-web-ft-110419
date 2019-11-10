6f8d7c483c7c05f32e8948f9a8184ec45824b469
Merkeresseria Coleman <kttsmllc@gmail.com>
2019-11-10 15:43:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def roll_call_dwarves = ["Doc","Dopey","Bashful","Grumpy"]
 end
 
 def summon_captain_planet = ["earth", "wind", "fire", "water", "heart"]
-  summon_captain_planet.each do |
+  summon_captain_planet.each do |summon_captain_planet|
     
 end
 
